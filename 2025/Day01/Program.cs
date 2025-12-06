333b1d46f6c48749a728b4bb66c0753636bffdaa
Alan McGovern <alan.mcgovern@gmail.com>
2025-12-06 14:21:00 +0000

    2025

---
+++ 2025/Day01/Program.cs
@@ -0,0 +1,33 @@
+﻿var input = File.ReadAllLines ("input.txt")
+    .Select (t => (t[0] == 'R' ? 1 : -1) * int.Parse (t.AsSpan ().Slice (1)))
+    .ToArray ();
+
+int zeroCount = 0;
+int dialValue = 50;
+
+foreach (var modification in input) {
+    dialValue += modification;
+    if (dialValue % 100 == 0)
+        zeroCount++;
+}
+
+Console.WriteLine ($"Q1: {zeroCount}");
+
+zeroCount = 0;
+dialValue = 50;
+
+foreach (var modification in input) {
+    for (int i =0; i < Math.Abs (modification); i ++) {
+        dialValue += Math.Sign (modification);
+        if (dialValue == 0) {
+            zeroCount++;
+        } else if (dialValue == -1) {
+            dialValue = 99;
+        } else if (dialValue == 100) {
+            dialValue = 0;
+            zeroCount++;
+        }
+    }
+}
+
+Console.WriteLine ($"Q2: {zeroCount}");
