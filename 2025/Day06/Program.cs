77b35d70db3da412865f34497b64461232dedc3f
Alan McGovern <alan.mcgovern@gmail.com>
2025-12-08 21:46:13 +0000

    This was a bit lazy
    Get it done, not get it done well ;)

--- 2025/Day06/Program.cs
+++ 2025/Day06/Program.cs
@@ -9,6 +9,7 @@ var numbers = input.SkipLast (1)
 var operators = input.Last ();
 
 Console.WriteLine ($"Q1: {ComputeTopwise (numbers, operators)}");
+Console.WriteLine ($"Q2: {ComputeTopwiseElite ()}");
 
 long ComputeTopwise (int[][] numbers, string[] operators)
 {
@@ -30,4 +31,60 @@ long ComputeTopwise (int[][] numbers, string[] operators)
     }
 
     return total;
+}
+
+static long ComputeTopwiseElite()
+{
+    var input = File.ReadAllLines ("input.txt")
+        .ToArray ();
+
+    // Add in a trailing separator
+    var numbers = input.SkipLast (1)
+        .Select (t => (t + " ").AsMemory ())
+        .ToArray ();
+
+    var operators = (input.Last () + " ").AsSpan ();
+
+    long total = 0;
+    while (operators.Length > 0) {
+        var nextNumber = operators.Slice (1).IndexOfAnyExcept (' ') + 1;
+        if (nextNumber == 0)
+            nextNumber = operators.Length;
+        var operatorSlice = operators.Slice (0, nextNumber);
+
+        total += Encephalopod (numbers, operatorSlice);
+
+        operators = operators.Slice (nextNumber);
+        for (int i = 0; i < numbers.Length; i++)
+            numbers[i] = numbers[i].Slice (nextNumber);
+    }
+
+    return total;
+}
+
+static long Encephalopod (ReadOnlyMemory<char>[] operandSlice, ReadOnlySpan<char> operatorSlice)
+{
+    // Subtract out the ' ' in between each group of numbers.
+    var length = operatorSlice.Length - 1;
+    var parts = operandSlice.Select (t => t.Slice (0, length)).ToArray ();
+
+    var op = operatorSlice.Slice (operatorSlice.IndexOfAnyExcept (' '), 1)[0];
+
+    long? total = null;
+    for (int i = length - 1; i >= 0; i--) {
+        long currentNumber = 0;
+        foreach (var part in parts) {
+            if (part.Span.Slice (i, 1)[0] != ' ')
+                currentNumber = currentNumber * 10 + long.Parse (part.Span.Slice (i, 1));
+        }
+        if (total == null)
+            total = currentNumber;
+        else if (op == '*')
+            total *= currentNumber;
+        else if (op == '+')
+            total += currentNumber;
+    }
+
+    Console.WriteLine ("  Was: {0}", total!.Value);
+    return total!.Value;
 }
