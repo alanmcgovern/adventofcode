38232a9f99ec26eb227c0a6282775f63a49b1803
Alan McGovern <alan.mcgovern@gmail.com>
2025-12-10 00:00:43 +0000

    I skipped 8. It seemed to algorithmy.
    Part 2 of 9 seems fun?

---
+++ 2025/Day09/Program.cs
@@ -0,0 +1,15 @@
+﻿var points = File.ReadAllLines ("input.txt")
+    .Select (t => t.Split (','))
+    .Select (t => (long.Parse (t[0]), long.Parse (t[1])))
+    .ToArray ();
+
+var coverage = new Dictionary<((long, long), (long, long)), long> ();
+for (int i = 0; i < points.Length; i ++) {
+    for (int j = i; j < points.Length; j++) {
+        var a = points[i];
+        var b = points[j];
+        coverage[(a, b)] = (1 + Math.Abs (b.Item1 - a.Item1)) * (1 + Math.Abs (b.Item2 - a.Item2));
+    }
+}
+
+Console.WriteLine ($"Q1: {coverage.Values.Max ()}");
